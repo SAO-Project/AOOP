5ed5b48c2b0b14161cc1b2a734fd584fe77986bf
Eddie Garcia <edd1e234g@gmail.com>
2020-10-26 12:47:24 -0600

    Eddie... First commit

---
+++ simple/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Optional;
+
+/**
+ * @author Edd1e234
+ * @version 1.0
+ * @since 10/26/20
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        Optional<String> hey = Optional.empty();
+    }
+}
