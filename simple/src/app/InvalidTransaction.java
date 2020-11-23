c810a008887b0680ed29609f4cb3f0c5038f0664
Eddie Garcia <edd1e234g@gmail.com>
2020-11-22 23:34:18 -0700

    Add Javadoc to exception class

--- simple/src/app/InvalidTransaction.java
+++ simple/src/app/InvalidTransaction.java
@@ -1,5 +1,12 @@
 package app;
 
+/**
+ * @author Alex Avila
+ * @since 11/22/20
+ * @version 1
+ *
+ * Custom exception class for transaction actions.
+ */
 public class InvalidTransaction extends RuntimeException {
     public InvalidTransaction(String errorMessage){
         super(errorMessage);
